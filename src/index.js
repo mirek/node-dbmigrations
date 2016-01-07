cb5ed243312cd5be5618e95657670a6ab71587bf
Mirek Rusin <mirek@me.com>
2016-01-07 23:06:17 +0100

    Export cli and update ignore list.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 
 import MigrationInfo from './migration-info';
 import Migrations from './migrations';
+import cli from './cli';
 
 export {
   MigrationInfo,
-  Migrations
+  Migrations,
+  cli
 };
